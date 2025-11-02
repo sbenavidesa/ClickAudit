a3a91713712fbde2577d1eedf7fcbfc82ecbd04d
Sergio <sergio.stack@pm.me>
2025-11-02 13:18:16 -0800

    improve evaluation flow

--- apps/user-application/worker/trpc/routers/evaluations.ts
+++ apps/user-application/worker/trpc/routers/evaluations.ts
@@ -1,31 +1,36 @@
 import { t } from "@/worker/trpc/trpc-instance";
 
 import { z } from "zod";
-import { EVALUATION_ISSUES, EVALUATIONS } from "./dummy-data";
+import {
+    getEvaluations,
+    getNotAvailableEvaluations,
+} from "@repo/data-ops/queries/evaluations";
 
 export const evaluationsTrpcRoutes = t.router({
-  problematicDestinations: t.procedure.query(async ({}) => {
-    return EVALUATION_ISSUES;
-  }),
-  recentEvaluations: t.procedure
-    .input(
-      z
-        .object({
-          createdBefore: z.string().optional(),
-        })
-        .optional(),
-    )
-    .query(async ({}) => {
-      const evaluations = EVALUATIONS;
-
-      const oldestCreatedAt =
-        evaluations.length > 0
-          ? evaluations[evaluations.length - 1].createdAt
-          : null;
-
-      return {
-        data: evaluations,
-        oldestCreatedAt,
-      };
+    problematicDestinations: t.procedure.query(async ({ ctx }) => {
+        return await getNotAvailableEvaluations(ctx.userInfo.userId);
     }),
+    recentEvaluations: t.procedure
+        .input(
+            z
+                .object({
+                    createdBefore: z.string().optional(),
+                })
+                .optional(),
+        )
+        .query(async ({ ctx }) => {
+            const evaluations = await getEvaluations(ctx.userInfo.userId);
+
+            const oldestCreatedAt =
+                evaluations.length > 0
+                    ? evaluations[evaluations.length - 1].createdAt
+                    : null;
+
+            return {
+                data: evaluations,
+                oldestCreatedAt,
+            };
+        }),
 });
+
+

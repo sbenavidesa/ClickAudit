b3b8388075aefceb93799336e08d97ec0d8e8898
Sergio <sergio.stack@pm.me>
2025-11-02 12:06:45 -0600

    preparing ai workers setup

--- apps/user-application/service-bindings.d.ts
+++ apps/user-application/service-bindings.d.ts
@@ -1,1 +1,9 @@
-interface ServiceBindings extends Env {}
+interface DestinationStatusEvaluationParams {
+    linkId: string;
+    destinationUrl: string;
+    accountId: string;
+}
+
+interface Env extends Cloudflare.Env {
+    DESTINATION_EVALUATION_WORKFLOW: Workflow<DestinationStatusEvaluationParams>;
+}

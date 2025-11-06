060c5c260c1d4433a07e800c3dd965b300581382
Sergio <sergio.stack@pm.me>
2025-11-06 16:16:15 -0600

    Update Api version

--- apps/user-application/src/components/auth/client.ts
+++ apps/user-application/src/components/auth/client.ts
@@ -3,6 +3,7 @@ import { stripeClient } from "@better-auth/stripe/client";
 
 
 export const authClient = createAuthClient({
+    baseURL: window.location.origin,
     plugins: [
         stripeClient({
             subscription: true,
